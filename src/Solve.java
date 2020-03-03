8e7ad7d53f5da02e0a469db21f863189f8efb69b
Alex Pelaez <pelaezalex8@gmail.com>
2020-03-02 17:16:13 -0700

    Test

--- src/Solve.java
+++ src/Solve.java
@@ -517,6 +517,7 @@ public class Solve {
     private int solveRecative(){
         boolean reachedGold = false;
         boolean smelledStench = false;
+//        help
         world1 = world;
         cost = 0;
         while (!reachedGold){
